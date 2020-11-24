76218fe6a8e9c9a227feb1e491b519199a2cafbe
cankut <cankutcoskun@sabanciuniv.edu>
2020-11-24 22:35:30 +0300

    mysql client demo

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -3,20 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+
+using MySql.Data.MySqlClient;
 
 namespace Server
 {
     static class Program
     {
+        //db.FILES 
+        public static void dumbDB()
+        {
+            string connstring = @"server=remotemysql.com;userid=ioI0xzbThf;password=VGITbQxEEa;database=ioI0xzbThf";
+
+            MySqlConnection conn = null;
+
+            try
+            {
+                conn = new MySqlConnection(connstring);
+                conn.Open();
+
+                string query = "SELECT * FROM FILES;";
+                MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "table_name");
+                DataTable dt = ds.Tables["table_name"];
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    foreach (DataColumn col in dt.Columns)
+                    {
+                        Console.Write(row[col] + "\t");
+                    }
+
+                    Console.Write("\n");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.ToString());
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            dumbDB();
+            /*
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+             */
+
         }
     }
 }
