655f0e30285a90f5645be71ca11bedc9e40216c7
giraycoskun <giraycoskun@sabanciuniv.edu>
2020-12-27 22:06:49 +0300

    updated GUI

--- Client/Client/Form1.Designer.cs
+++ Client/Client/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CLIENT));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -50,14 +51,19 @@
             this.copyButton = new System.Windows.Forms.Button();
             this.getFileButton = new System.Windows.Forms.Button();
             this.changeAccessButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(738, 9);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14F);
+            this.label1.Location = new System.Drawing.Point(683, 76);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.Size = new System.Drawing.Size(130, 49);
             this.label1.TabIndex = 0;
             this.label1.Text = "CLIENT";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -65,9 +71,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(95, 68);
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(47, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.Size = new System.Drawing.Size(78, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Server IP:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
@@ -76,88 +83,107 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(97, 122);
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(47, 85);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.Size = new System.Drawing.Size(100, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "Port Number:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // outputBox
             // 
-            this.outputBox.Location = new System.Drawing.Point(70, 202);
+            this.outputBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputBox.Location = new System.Drawing.Point(20, 153);
             this.outputBox.Name = "outputBox";
-            this.outputBox.Size = new System.Drawing.Size(676, 390);
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(530, 430);
             this.outputBox.TabIndex = 3;
             this.outputBox.Text = "";
             // 
             // ipBox
             // 
-            this.ipBox.Location = new System.Drawing.Point(257, 68);
+            this.ipBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ipBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ipBox.Location = new System.Drawing.Point(202, 28);
             this.ipBox.Name = "ipBox";
-            this.ipBox.Size = new System.Drawing.Size(200, 26);
+            this.ipBox.Size = new System.Drawing.Size(141, 35);
             this.ipBox.TabIndex = 4;
             this.ipBox.TextChanged += new System.EventHandler(this.ipBox_TextChanged);
             // 
             // portBox
             // 
-            this.portBox.Location = new System.Drawing.Point(257, 116);
+            this.portBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.portBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.portBox.Location = new System.Drawing.Point(202, 69);
             this.portBox.Name = "portBox";
-            this.portBox.Size = new System.Drawing.Size(200, 26);
+            this.portBox.Size = new System.Drawing.Size(141, 35);
             this.portBox.TabIndex = 5;
             // 
             // connectButton
             // 
-            this.connectButton.Location = new System.Drawing.Point(526, 68);
+            this.connectButton.BackColor = System.Drawing.SystemColors.Info;
+            this.connectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectButton.Location = new System.Drawing.Point(407, 28);
             this.connectButton.Name = "connectButton";
-            this.connectButton.Size = new System.Drawing.Size(182, 45);
+            this.connectButton.Size = new System.Drawing.Size(118, 43);
             this.connectButton.TabIndex = 6;
             this.connectButton.Text = "CONNECT";
-            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.UseVisualStyleBackColor = false;
             this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
             // 
             // stopButton
             // 
-            this.stopButton.Location = new System.Drawing.Point(526, 136);
+            this.stopButton.BackColor = System.Drawing.SystemColors.Info;
+            this.stopButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.stopButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stopButton.Location = new System.Drawing.Point(407, 104);
             this.stopButton.Name = "stopButton";
-            this.stopButton.Size = new System.Drawing.Size(182, 43);
+            this.stopButton.Size = new System.Drawing.Size(118, 41);
             this.stopButton.TabIndex = 7;
             this.stopButton.Text = "STOP";
-            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.UseVisualStyleBackColor = false;
             this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
             // 
             // usernameBox
             // 
-            this.usernameBox.Location = new System.Drawing.Point(253, 158);
+            this.usernameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usernameBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.usernameBox.Location = new System.Drawing.Point(202, 110);
             this.usernameBox.Name = "usernameBox";
-            this.usernameBox.Size = new System.Drawing.Size(205, 26);
+            this.usernameBox.Size = new System.Drawing.Size(141, 35);
             this.usernameBox.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(97, 167);
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(47, 126);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.Size = new System.Drawing.Size(77, 19);
             this.label4.TabIndex = 9;
             this.label4.Text = "Username";
             // 
             // uploadFileBox
             // 
-            this.uploadFileBox.Location = new System.Drawing.Point(946, 154);
+            this.uploadFileBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uploadFileBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.uploadFileBox.Location = new System.Drawing.Point(635, 224);
             this.uploadFileBox.Name = "uploadFileBox";
-            this.uploadFileBox.Size = new System.Drawing.Size(240, 26);
+            this.uploadFileBox.Size = new System.Drawing.Size(198, 35);
             this.uploadFileBox.TabIndex = 11;
             this.uploadFileBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // browseButton
             // 
-            this.browseButton.Location = new System.Drawing.Point(946, 195);
+            this.browseButton.BackColor = System.Drawing.SystemColors.Info;
+            this.browseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.browseButton.Location = new System.Drawing.Point(635, 265);
             this.browseButton.Name = "browseButton";
-            this.browseButton.Size = new System.Drawing.Size(240, 29);
+            this.browseButton.Size = new System.Drawing.Size(198, 27);
             this.browseButton.TabIndex = 12;
             this.browseButton.Text = "Choose File";
-            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.UseVisualStyleBackColor = false;
             this.browseButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // openFileDialog1
@@ -167,83 +193,111 @@
             // 
             // uploadButton
             // 
-            this.uploadButton.Location = new System.Drawing.Point(873, 266);
+            this.uploadButton.BackColor = System.Drawing.SystemColors.Info;
+            this.uploadButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.uploadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.uploadButton.Location = new System.Drawing.Point(601, 312);
             this.uploadButton.Name = "uploadButton";
-            this.uploadButton.Size = new System.Drawing.Size(162, 45);
+            this.uploadButton.Size = new System.Drawing.Size(120, 43);
             this.uploadButton.TabIndex = 13;
             this.uploadButton.Text = "UPLOAD";
-            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.UseVisualStyleBackColor = false;
             this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
             // 
             // downloadFolderBox
             // 
-            this.downloadFolderBox.Location = new System.Drawing.Point(1083, 545);
+            this.downloadFolderBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.downloadFolderBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.downloadFolderBox.Location = new System.Drawing.Point(590, 548);
             this.downloadFolderBox.Name = "downloadFolderBox";
-            this.downloadFolderBox.Size = new System.Drawing.Size(251, 26);
+            this.downloadFolderBox.Size = new System.Drawing.Size(291, 35);
             this.downloadFolderBox.TabIndex = 14;
             // 
             // browseFolderButton
             // 
-            this.browseFolderButton.Location = new System.Drawing.Point(798, 545);
+            this.browseFolderButton.BackColor = System.Drawing.SystemColors.Info;
+            this.browseFolderButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.browseFolderButton.Location = new System.Drawing.Point(635, 499);
             this.browseFolderButton.Name = "browseFolderButton";
-            this.browseFolderButton.Size = new System.Drawing.Size(240, 33);
+            this.browseFolderButton.Size = new System.Drawing.Size(198, 32);
             this.browseFolderButton.TabIndex = 15;
             this.browseFolderButton.Text = "Choose Download Folder";
-            this.browseFolderButton.UseVisualStyleBackColor = true;
+            this.browseFolderButton.UseVisualStyleBackColor = false;
             this.browseFolderButton.Click += new System.EventHandler(this.browseFolderButton_Click);
             // 
             // downloadButton
             // 
-            this.downloadButton.Location = new System.Drawing.Point(1120, 266);
+            this.downloadButton.BackColor = System.Drawing.SystemColors.Info;
+            this.downloadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.downloadButton.Location = new System.Drawing.Point(772, 312);
             this.downloadButton.Name = "downloadButton";
-            this.downloadButton.Size = new System.Drawing.Size(162, 45);
+            this.downloadButton.Size = new System.Drawing.Size(120, 43);
             this.downloadButton.TabIndex = 17;
             this.downloadButton.Text = "DOWNLOAD";
-            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.UseVisualStyleBackColor = false;
             this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
             // 
             // deleteButton
             // 
-            this.deleteButton.Location = new System.Drawing.Point(873, 335);
+            this.deleteButton.BackColor = System.Drawing.SystemColors.Info;
+            this.deleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteButton.Location = new System.Drawing.Point(601, 361);
             this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(162, 44);
+            this.deleteButton.Size = new System.Drawing.Size(120, 42);
             this.deleteButton.TabIndex = 18;
             this.deleteButton.Text = "DELETE";
-            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.UseVisualStyleBackColor = false;
             // 
             // copyButton
             // 
-            this.copyButton.Location = new System.Drawing.Point(1120, 335);
+            this.copyButton.BackColor = System.Drawing.SystemColors.Info;
+            this.copyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.copyButton.Location = new System.Drawing.Point(772, 361);
             this.copyButton.Name = "copyButton";
-            this.copyButton.Size = new System.Drawing.Size(162, 44);
+            this.copyButton.Size = new System.Drawing.Size(120, 42);
             this.copyButton.TabIndex = 19;
             this.copyButton.Text = "COPY";
-            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.UseVisualStyleBackColor = false;
             this.copyButton.Click += new System.EventHandler(this.button4_Click);
             // 
             // getFileButton
             // 
-            this.getFileButton.Location = new System.Drawing.Point(937, 69);
+            this.getFileButton.BackColor = System.Drawing.SystemColors.Info;
+            this.getFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.getFileButton.Location = new System.Drawing.Point(635, 153);
             this.getFileButton.Name = "getFileButton";
-            this.getFileButton.Size = new System.Drawing.Size(249, 48);
+            this.getFileButton.Size = new System.Drawing.Size(198, 46);
             this.getFileButton.TabIndex = 20;
             this.getFileButton.Text = "GET FILES";
-            this.getFileButton.UseVisualStyleBackColor = true;
+            this.getFileButton.UseVisualStyleBackColor = false;
             // 
             // changeAccessButton
             // 
-            this.changeAccessButton.Location = new System.Drawing.Point(1004, 408);
+            this.changeAccessButton.BackColor = System.Drawing.SystemColors.Info;
+            this.changeAccessButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.changeAccessButton.Location = new System.Drawing.Point(635, 409);
             this.changeAccessButton.Name = "changeAccessButton";
-            this.changeAccessButton.Size = new System.Drawing.Size(162, 44);
+            this.changeAccessButton.Size = new System.Drawing.Size(198, 42);
             this.changeAccessButton.TabIndex = 21;
             this.changeAccessButton.Text = "CHANGE ACCESS";
-            this.changeAccessButton.UseVisualStyleBackColor = true;
+            this.changeAccessButton.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Client.Properties.Resources.cloud;
+            this.pictureBox1.Location = new System.Drawing.Point(635, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
             // 
             // CLIENT
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1373, 618);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(930, 599);
             this.Controls.Add(this.changeAccessButton);
             this.Controls.Add(this.getFileButton);
             this.Controls.Add(this.copyButton);
@@ -264,9 +318,14 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CLIENT";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CLIENT";
             this.Load += new System.EventHandler(this.CLIENT_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -296,6 +355,7 @@
         private System.Windows.Forms.Button copyButton;
         private System.Windows.Forms.Button getFileButton;
         private System.Windows.Forms.Button changeAccessButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
